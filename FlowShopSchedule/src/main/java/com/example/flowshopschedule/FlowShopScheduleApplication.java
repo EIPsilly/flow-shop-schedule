113f2062cb25c6fe519b4899c7e065f01e61aaa2
EIPsilly <45347422+EIPsilly@users.noreply.github.com>
2022-06-22 19:24:32 +0800

    创建项目

---
+++ FlowShopSchedule/src/main/java/com/example/flowshopschedule/FlowShopScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowshopschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowShopScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowShopScheduleApplication.class, args);
+    }
+
+}
---
+++ FlowShopSchedule/src/test/java/com/example/flowshopschedule/FlowShopScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flowshopschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowShopScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
